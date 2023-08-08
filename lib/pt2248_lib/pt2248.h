2ce34ae297fdc171a32c36335e55e554ca0bb46a
Nguyễn Minh Quân <minyingkupo@gmail.com>
2023-08-08 17:35:09 +0700

    test

--- lib/pt2248_lib/pt2248.h
+++ lib/pt2248_lib/pt2248.h
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <Arduino.h>
 #include <stdio.h>
+#include <IRremote.h>
 
 typedef struct {
     uint16_t POWER_ON_PIN;
@@ -11,18 +12,31 @@ typedef struct {
     uint16_t DATA_STATE ;
 } st_pt2248;
 
-#define PT2248_OSC_CLOCK_PERIOD       (422) 
-#define PT2248_CLK_PERIOD_3           (PT2248_OSC_CLOCK_PERIOD*3)
-#define PT2248_CLK_PERIOD_60          (PT2248_OSC_CLOCK_PERIOD*60)
-#define PT2248_CLK_PERIOD_80          (PT2248_OSC_CLOCK_PERIOD*60)
 
-
+const int irLedPin = 11;       // setup ir
+IRsend irsend(irLedPin);
+unsigned int sonyCode = 0x15A;
 
 /*
  * PT2248/2249 has 2 types of bit
  */
 #define PT2248_BIT_0                  (0)
 #define PT2248_BIT_1                  (1)
+
+const int carrierFrequency = 38000; // Tần số sóng mang là 38kHz
+// Hằng số thời gian truyền mỗi bit
+const unsigned long bitDuration =  1000000 / carrierFrequency;
+
+// Dãy mang cua command Word cua cac key no : 7,8, 9
+bool dataBits7[] = {1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0};
+
+bool dataBits8[] = {1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0};
+
+bool dataBits9[] = {1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0};
+
+bool dataBits10[] = {1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0};
+
+
 
 void pt2248Init(st_pt2248 *PT2248, uint16_t data_out_pin, uint16_t data_input_pin0, uint16_t data_input_pin1, uint16_t data_input_pin2, uint16_t data_input_pin3)
 {
@@ -34,13 +48,15 @@ void pt2248Init(st_pt2248 *PT2248, uint16_t data_out_pin, uint16_t data_input_pi
 
   
   pinMode(PT2248->DATA_OUT_PIN    , OUTPUT );
-  pinMode(PT2248->DATA_INPUT_PIN[0], INPUT  );
-  pinMode(PT2248->DATA_INPUT_PIN[1], INPUT  );
-  pinMode(PT2248->DATA_INPUT_PIN[2], INPUT  );
-  pinMode(PT2248->DATA_INPUT_PIN[3], INPUT  );
+  pinMode(PT2248->DATA_INPUT_PIN[0], INPUT_PULLUP  );
+  pinMode(PT2248->DATA_INPUT_PIN[1], INPUT_PULLUP  );
+  pinMode(PT2248->DATA_INPUT_PIN[2], INPUT_PULLUP  );
+  pinMode(PT2248->DATA_INPUT_PIN[3], INPUT_PULLUP  );
 
 }
 
+
+/*
 void sendBit(st_pt2248 *PT2248, uint16_t PT2248_BIT){
     switch (PT2248_BIT) {
         case PT2248_BIT_0: // Bit 0: 1 LOW -> 3 HIGH
@@ -60,22 +76,49 @@ void sendBit(st_pt2248 *PT2248, uint16_t PT2248_BIT){
         break;
     }
 }
+*/
 
+void sendBit(bool bitValue) {
+  if (bitValue) {
+    irsend.mark(bitDuration);
+    irsend.space(bitDuration * 3);
+  } else {
+    irsend.mark(bitDuration);
+    irsend.space(bitDuration);
+    irsend.mark(bitDuration * 3);
+  }
+}
+
+/*
 void single_short(st_pt2248 *PT2248, uint16_t dataCode){
     // 2 Command words
     delayMicroseconds(PT2248_CLK_PERIOD_60);
 
     //Command code
     for(uint8_t i=0; i<12; i++){
-            uint8_t bitValue = (dataCode >> i) & 0x001;
+            uint8_t bitValue = (dataCode >> (11-i)) & 0x001;
             sendBit(PT2248, bitValue);
         }
     delayMicroseconds(PT2248_CLK_PERIOD_80);
+
+    for(uint8_t i=0; i<12; i++){
+            uint8_t bitValue = (dataCode >> (11-i)) & 0x001;
+            sendBit(PT2248, bitValue);
+        }
+}
+*/
+
+// Hàm gửi SINGLE-SHOT SIGNAL
+void sendSingleShotSignal(bool dataBits[]) {
+  // Gửi từng bit trong dãy
+  for (int i = 0; i < 12; i++) {
+    sendBit(dataBits[i]);
+  }
 }
 
 void getState(st_pt2248 *PT2248){
-    PT2248->DATA_STATE = 0x000;
-    
+
+    PT2248->DATA_STATE = 0x000; 
      if( digitalRead(PT2248->DATA_INPUT_PIN[0]) != 0 )
        PT2248->DATA_STATE = 0b100010100000  ;    //key 7
      if( digitalRead(PT2248->DATA_INPUT_PIN[1]) != 0 )
@@ -85,3 +128,6 @@ void getState(st_pt2248 *PT2248){
      if( digitalRead(PT2248->DATA_INPUT_PIN[3]) != 0 )
        PT2248->DATA_STATE = 0b100010000100  ;    //key 10
 }
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+/*
 #include <Arduino.h>
 #include "pt2248.h"
 #include <stdio.h>
@@ -6,18 +7,116 @@ st_pt2248 pPT2248;
 
 void setup() {
   Serial.begin(9600);
-  pt2248Init(&pPT2248, 10, 2, 4, 6, 8);
-  //Serial.begin(9600);
+  pt2248Init(&pPT2248, 11, 2, 4, 6, 8);
 }
 
 void loop() {
     getState(&pPT2248);
-    if (pPT2248.DATA_STATE != 0)
-        single_short(&pPT2248, pPT2248.DATA_STATE);
-    //int state = digitalRead(pPT2248.DATA_INPUT_PIN[0]); // Đọc trạng thái của chân pin
-    int state = digitalRead(pPT2248.DATA_OUT_PIN); // Đọc trạng thái của chân pin
-    Serial.println(state);              //Xuất trạng thái
+    if (pPT2248.DATA_STATE != 0) {
+      single_short(&pPT2248, pPT2248.DATA_STATE);
+    }
+    int state = digitalRead(pPT2248.DATA_INPUT_PIN[0]); // Đọc trạng thái của chân pin
+    Serial.println(state);
     //delay(250);
   
     pPT2248.DATA_STATE = 0;
 }
+*/
+
+#include <Arduino.h>
+//#include "pt2248.h"
+#include <stdio.h>
+#include <IRremote.h>
+
+
+st_pt2248 pPT2248;
+
+int singleButtonState7 = HIGH;   // Trạng thái trước của nút bấm single
+int singleButtonState8 = HIGH;   // Trạng thái trước của nút bấm single
+int singleButtonState9 = HIGH;   // Trạng thái trước của nút bấm single
+int singleButtonState10 = HIGH;   // Trạng thái trước của nút bấm single
+
+
+
+
+void setup() {
+  Serial.begin(9600);
+  pt2248Init(&pPT2248, 11, 2, 4, 6, 8);
+}
+
+void loop() {
+  int currentSingle7 = pPT2248.DATA_INPUT_PIN[0];
+
+  int currentSingle8 = pPT2248.DATA_INPUT_PIN[1];
+
+  int currentSingle9 = pPT2248.DATA_INPUT_PIN[2];
+
+  int currentSingle10 = pPT2248.DATA_INPUT_PIN[3];
+
+
+  // Ham gui tin hieu single shot khi nhan nut o day voi command Word la 7: 
+  if(currentSingle7 != singleButtonState7){
+    if(currentSingle7 == LOW){
+      Serial.print("dung");
+      //digitalWrite(irLedPin, HIGH);
+      //sendSingleShotSignal(dataBits7);
+      //sendSingleShotSignal(dataBits7);
+      irsend.sendSony(sonyCode, 12);
+    }else{
+      //digitalWrite(irLedPin, LOW);
+      noTone(pPT2248.DATA_OUT_PIN); // Dừng lại tín hiệu PWM
+    }
+  }
+
+  // Ham gui tin hieu single shot khi nhan nut o day voi command Word la 8: 
+  if(currentSingle8 != singleButtonState8){
+    if(currentSingle8 == LOW){
+      //digitalWrite(irLedPin, HIGH);
+      //{1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0}
+      unsigned int rawData[] = {420, 1262, 420, 1262, 420, 1262, 420, 1262, 1262, 420, 420, 1262, 420, 1262, 1262, 420, 420, 1262, 420, 1262, 420, 1262, 420, 1262};
+      //unsigned int rawData[] = {1600, 1600, 560, 1600, 560, 560, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560};
+      irsend.sendRaw(rawData, 12, 38);
+      //sendSingleShotSignal(dataBits8);
+      //sendSingleShotSignal(dataBits9);
+    }else{
+      noTone(pPT2248.DATA_OUT_PIN); // Dừng lại tín hiệu PWM   
+    }
+  }
+  
+  if(currentSingle9 != singleButtonState9){
+    if(currentSingle9 == LOW){
+      //digitalWrite(irLedPin, HIGH);
+      //{1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0}
+      unsigned int rawData[] = {420, 1262, 420, 1262, 420, 1262, 420, 1262, 1262, 420, 420, 1262, 420, 1262, 1262, 420, 420, 1262, 420, 1262, 420, 1262, 420, 1262};
+      //unsigned int rawData[] = {1600, 1600, 560, 1600, 560, 560, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560};
+      irsend.sendRaw(rawData, 12, 38);
+      //sendSingleShotSignal(dataBits8);
+      //sendSingleShotSignal(dataBits9);
+    }else{
+      noTone(pPT2248.DATA_OUT_PIN); // Dừng lại tín hiệu PWM   
+    }
+  }
+
+  if(currentSingle10 != singleButtonState10){
+    if(currentSingle10 == LOW){
+      //digitalWrite(irLedPin, HIGH);
+      //{1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0}
+      unsigned int rawData[] = {420, 1262, 420, 1262, 420, 1262, 420, 1262, 1262, 420, 420, 1262, 420, 1262, 1262, 420, 420, 1262, 420, 1262, 420, 1262, 420, 1262};
+      //unsigned int rawData[] = {1600, 1600, 560, 1600, 560, 560, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560, 1600, 560};
+      irsend.sendRaw(rawData, 12, 38);
+      //sendSingleShotSignal(dataBits8);
+      //sendSingleShotSignal(dataBits9);
+    }else{
+      noTone(pPT2248.DATA_OUT_PIN); // Dừng lại tín hiệu PWM   
+    }
+  }
+  /*
+  * Cap nhat trang thai truoc cua dau 6 va dau 7
+  * Lay thong tin tu cac chan 6 va 7 cua vi dieu khien 
+  */ 
+  singleButtonState7 = currentSingle7;
+  singleButtonState8 = currentSingle8;
+  singleButtonState9 = currentSingle9;
+  singleButtonState10 = currentSingle10;
+
+  }
